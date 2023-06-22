841984f1bc7d8d4d5bf18c3e8bc2dc797d4e04aa
arth <arth@globaltllc.com>
2023-06-22 15:57:31 +0800

    added schemas for chat, message, and user

---
+++ backend/models/MessageModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    content: { type: String, trim: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
