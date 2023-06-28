b1a71c5238699512e89fc73e8a75ea01ded80fd9
arth <arth@globaltllc.com>
2023-06-28 18:48:46 +0800

    auth middleware added

---
+++ backend/controllers/chatControllers.js
@@ -0,0 +1,4 @@
+const asyncHandler = require("express-async-handler");
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+});
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const { chats } = require("./data/chats");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config;
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chats", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
